c09b7c3ef953880679205d712b7cc6eec18a6978
Salman S <salmanshalahuddin133@gmail.com>
2021-03-17 16:19:31 +0700

    final update polymorphic sorting

--- Polymorphic Sorting/Sorting.java
+++ Polymorphic Sorting/Sorting.java
@@ -33,8 +33,7 @@ public class Sorting  {
 		for (int index = 1; index < list.length; index++)   {  
 			Comparable key = list[index];  
 			int position = index;  
-			// Shift larger values to the right  
-			while (position > 0 && key.compareTo(list[position-1]) < 0)   {  
+			while (position > 0 && key.compareTo(list[position-1]) > 0)   {  
 				list[position] = list[position-1];  
 				position--;  
 			}  
