ffd9ce75d65c92865d6d96b9e4a255f8a44a7e59
Salman S <salmanshalahuddin133@gmail.com>
2021-03-26 13:22:13 +0700

    final update read & write in file

--- Week 2/ReadWriteFile/Warning.java
+++ Week 2/ReadWriteFile/Warning.java
@@ -16,29 +16,38 @@ public class Warning  {
 	public static void main (String[] args)  {  
 		int creditHrs; // number of semester hours earned   
 		double qualityPts; // number of quality points earned   
-		double gpa; // grade point (quality point) average  
-		String line, name, inputName = "students.dat";  
-		String outputName = "warning.dat"; 
+		double gpa; // grade point (quality point) average 
+		String[] line;
+		String name, inputName = "D:\\eclipse-workspace\\Week2_APPL1\\src\\ReadWriteFile\\students.dat";  
+		String outputName = "D:\\eclipse-workspace\\Week2_APPL1\\src\\ReadWriteFile\\warning.dat"; 
 		try  {  
-			// Set up scanner to input file  
-			// Set up the output file stream  
+			Scanner scan = new Scanner(new File(inputName)); // Set up scanner to input file  
+			PrintWriter outFile = new PrintWriter(outputName); // Set up the output file stream  
 			// Print a header to the output file  
 			outFile.println ();  
 			outFile.println ("Students on Academic Warning");  
 			outFile.println ();  
 			// Process the input file, one token at a time    
-			while ()  {  
-				// Get the credit hours and quality points and   
-				// determine if the student is on warning. If so,   
+			while (scan.hasNextLine())  {  
+				line = scan.nextLine().split(" "); // Menampung data per line dalam array dipisah dengan spasi
+				// Get the credit hours and quality points and
+				// determine if the student is on warning. If so,				
 				// write the student data to the output file.  
+				name = line[0];
+				// Parsing digunakan agar bentuk string dari file dapat ditampung menjadi tipe data yang dibutuhkan
+				creditHrs = Integer.parseInt(line[1]);
+				qualityPts = Double.parseDouble(line[2]);
+				gpa = qualityPts/creditHrs;
+				
+				if ((gpa<1.5 && creditHrs<30) || (gpa<1.75 && creditHrs<60) || (gpa<2)) {
+					outFile.println(name + " " + creditHrs + " " + gpa);
+				}
 		}  
-			// Close output file  
+			outFile.close(); // Close output file  
+			System.out.println("Filter Success, check the output file");
 		}  
 		catch (FileNotFoundException exception)  {  
 			System.out.println ("The file " + inputName + " was not found.");   
-		}  
-		catch (IOException exception)  {  
-			System.out.println (exception);  
 		}  
 		catch (NumberFormatException e)  {  
 			System.out.println ("Format error in input file: " + e);   
