27a244eb80c583da1bfb71db46a85d93e8621664
Salman S <salmanshalahuddin133@gmail.com>
2021-03-27 15:19:56 +0700

    update final moveable circle

--- Week 2/EnhancingMoveableCircle/CirclePanel.java
+++ Week 2/EnhancingMoveableCircle/CirclePanel.java
@@ -10,47 +10,65 @@ import java.awt.*;
 import javax.swing.*;  
 import java.awt.event.*;  
 public class CirclePanel extends JPanel  {  
-private final int CIRCLE_SIZE = 50;  
-private int x,y;  
-private Color c;  
-//---------------------------------------------------------------   
-// Set up circle and buttons to move it.  
-//---------------------------------------------------------------   
-public CirclePanel(int width, int height)  {  
-	// Set coordinates so circle starts in middle  
-	x = (width/2)-(CIRCLE_SIZE/2);  
-	y = (height/2)-(CIRCLE_SIZE/2);  
-	c = Color.green;  
-	// Need a border layout to get the buttons on the bottom   
-	this.setLayout(new BorderLayout());  
+	private final int CIRCLE_SIZE = 50;  
+	private int x,y;  
+	private Color c;  
+	
+	private int width, height;
 	// Create buttons to move the circle  
 	JButton left = new JButton("Left");  
 	JButton right = new JButton("Right");  
 	JButton up = new JButton("Up");  
-	JButton down = new JButton("Down");  
-	// Add listeners to the buttons  
-	left.addActionListener(new MoveListener(-20,0));   
-	right.addActionListener(new MoveListener(20,0));   
-	up.addActionListener(new MoveListener(0,-20));   
-	down.addActionListener(new MoveListener(0,20));  
-	// Need a panel to put the buttons on or they'll be on   
-	// top of each other.  
-	JPanel buttonPanel = new JPanel();  
-	buttonPanel.add(left);  
-	buttonPanel.add(right);  
-	buttonPanel.add(up);  
-	buttonPanel.add(down);  
-	// Add the button panel to the bottom of the main panel   
-	this.add(buttonPanel, "South");  
-}  
+	JButton down = new JButton("Down"); 
 	//---------------------------------------------------------------   
-// Draw circle on CirclePanel  
+	// Set up circle and buttons to move it.  
 	//---------------------------------------------------------------   
+	public CirclePanel(int width, int height)  {  
+		this.width = width;
+		this.height = height;
+		
+		// Set coordinates so circle starts in middle  
+		x = (width/2)-(CIRCLE_SIZE/2);  
+		y = (height/2)-(CIRCLE_SIZE/2);  
+		c = Color.green;  
+		// Need a border layout to get the buttons on the bottom   
+		this.setLayout(new BorderLayout());  
+		 
+		// Add listeners to the buttons  
+		left.addActionListener(new MoveListener(-20,0));   
+		right.addActionListener(new MoveListener(20,0));   
+		up.addActionListener(new MoveListener(0,-20));   
+		down.addActionListener(new MoveListener(0,20));  
+		// Need a panel to put the buttons on or they'll be on   
+		// top of each other.  
+		JPanel buttonPanel = new JPanel();  
+		buttonPanel.add(left);  
+		buttonPanel.add(right);  
+		buttonPanel.add(up);  
+		buttonPanel.add(down);  
+		// Add the button panel to the bottom of the main panel
+		this.add(buttonPanel, "South");  	
+		// Mnemonics
+		left.setMnemonic(KeyEvent.VK_L);
+		right.setMnemonic(KeyEvent.VK_R);
+		up.setMnemonic(KeyEvent.VK_U);
+		down.setMnemonic(KeyEvent.VK_D);
+		// Tooltips
+		left.setToolTipText("Move circle to the left 20px");
+		right.setToolTipText("Move circle to the right 20px");
+		up.setToolTipText("Circle moved up 20px");
+		down.setToolTipText("Circle moved down 20px");
+	}  
+	
+	//---------------------------------------------------------------   
+	// Draw circle on CirclePanel  
+	//--------------------------------------------------------------- 
 	public void paintComponent(Graphics page)  { 
 		super.paintComponent(page);  
 		page.setColor(c);  
 		page.fillOval(x,y,CIRCLE_SIZE,CIRCLE_SIZE);  
 	}  
+	
 	//---------------------------------------------------------------   
 	// Class to listen for button clicks that move circle.   
 	//---------------------------------------------------------------  
@@ -70,7 +88,33 @@ public CirclePanel(int width, int height)  {
 		public void actionPerformed(ActionEvent e)  {  
 			x += dx;  
 			y += dy;  
-			repaint();  
+			repaint();
+			
+			// Enable / Disable Buttons ketika berada di ujung layar
+			if (x<=0) {
+				left.setEnabled(false);
+			}
+			else {
+				left.setEnabled(true);
+			}
+			if (x>width-CIRCLE_SIZE) {
+				right.setEnabled(false);
+			}
+			else {
+				right.setEnabled(true);
+			}
+			if (y<=0) {
+				up.setEnabled(false);
+			}
+			else {
+				up.setEnabled(true);
+			}
+			if (y>=height-CIRCLE_SIZE) {
+				down.setEnabled(false);
+			}
+			else {
+				down.setEnabled(true);
+			}
 		}  
 	}  
 } 
